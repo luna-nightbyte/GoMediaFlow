ca0905eaae70b784286e3c74e939b2a47c30655c
luna-nightbyte <lunawifu18@gmail.com>
2025-01-25 20:24:20 +0100

    input ip and port to enable multiple connections

--- modules/web/connect.go
+++ modules/web/connect.go
@@ -2,7 +2,7 @@ package web
 
 import (
 	"fmt"
-	"goStreamer/modules/config"
+	"goStreamer/modules/files"
 	"log"
 	"net"
 	"sync"
@@ -11,15 +11,16 @@ import (
 type Server struct {
 	Conn  net.Conn
 	WG    sync.WaitGroup
-	Files output
+	Files files.Output
 }
 
-func (s *Server) Connect() {
-	// Prepare a connection to the remote computer
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", config.Config.IP, config.Config.PORT))
+// Connect establishes a connection to the remote server.
+// Blocks until connection is established
+func (s *Server) Connect(ip string, port int) {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
 	if err != nil {
 		log.Fatalf("Failed to connect to remote computer: %v", err)
 	}
 	s.Conn = conn
-
+	log.Println("Connected to server:", ip, port)
 }
